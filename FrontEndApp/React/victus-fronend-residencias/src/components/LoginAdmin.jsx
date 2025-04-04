09d4e9b0624f9cf2cfb98a9645d94adb7b1679cc
Andres Velez <andresjobpk@gmail.com>
2025-04-04 07:47:59 -0500

    boton bien puesto para regresar a HOME en loginAdmin

--- FrontEndApp/React/victus-fronend-residencias/src/components/LoginAdmin.jsx
+++ FrontEndApp/React/victus-fronend-residencias/src/components/LoginAdmin.jsx
@@ -5,43 +5,46 @@ const LoginAdmin = () => {
     return (
         <>
             <Header></Header>
-            <div className="container">
-                <h2 className="text-initial">
-                    Iniciar Sesión 'Como Admin'
-                </h2>
-                <form>
-                    <div className="information">
-                        <label className="textInput" htmlFor="email">
-                            Correo o usuario
-                        </label>
-                        <input id="email" type="text" />
-                    </div>
-                    <div className="information">
-                        <label className="textInput" htmlFor="password">
-                            Contraseña
-                        </label>
-                        <input id="password" type="password" />
-                    </div>
-                    <div className="linksWithButton">
-                        {/* <a className="linkRegister" href="#">
+            <div className="containerFather">
+                <Link className="ButtonAccept" to={"/github-pages"}>
+                    Home
+                </Link>
+                <div className="container">
+                    <h2 className="text-initial">
+                        Iniciar Sesión 'Como Admin'
+                    </h2>
+                    <form>
+                        <div className="information">
+                            <label className="textInput" htmlFor="email">
+                                Correo o usuario
+                            </label>
+                            <input id="email" type="text" />
+                        </div>
+                        <div className="information">
+                            <label className="textInput" htmlFor="password">
+                                Contraseña
+                            </label>
+                            <input id="password" type="password" />
+                        </div>
+                        <div className="linksWithButton">
+                            {/* <a className="linkRegister" href="#">
                             Registrarse
                         </a> */}
-                        <Link className="ButtonAccept" to={"/github-pages"}>
-                            Home
-                        </Link>
-                        <Link className="ButtonAccept" to={""}>
-                            iniciarFacil
-                        </Link>
-                        <button className="ButtonAccept" type="submit">
-                            Aceptar
-                        </button>
-                        <a className="linkForgetPassword" href="#">
-                            ¿Olvidó la contraseña?
-                        </a>
-                    </div>
 
-                </form>
+                            <Link className="ButtonAccept" to={""}>
+                                iniciarFacil
+                            </Link>
+                            <button className="ButtonAccept" type="submit">
+                                Aceptar
+                            </button>
+                            <a className="linkForgetPassword" href="#">
+                                ¿Olvidó la contraseña?
+                            </a>
+                        </div>
 
+                    </form>
+
+                </div>
             </div>
         </>
     )
