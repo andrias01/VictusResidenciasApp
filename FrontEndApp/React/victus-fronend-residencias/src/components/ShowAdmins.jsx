aff7bf8dad2c469f1b27ef662fdf270551b4afea
Andres Velez <andresjobpk@gmail.com>
2025-04-06 21:17:21 -0500

    Dashboard Admin listo

--- FrontEndApp/React/victus-fronend-residencias/src/components/ShowAdmins.jsx
+++ FrontEndApp/React/victus-fronend-residencias/src/components/ShowAdmins.jsx
@@ -1,13 +1,15 @@
 import { useContext } from "react"
 import Header from "./Header"
-import { AdminContext } from "../contexts/admin.context.jsx";
+// import { AdminContext } from "../contexts/admin.context.jsx";
+import AdminDashboard from "./AdminDashboard.jsx"
 function ShowAdmins() {
-  const {HTMLAdmins} = useContext(AdminContext);
+  // const {HTMLAdmins} = useContext(AdminContext);
   //const {getAdmins} = useContext(AdminContext);
   return (
     <>
     <Header></Header>
-    <ul>{HTMLAdmins}</ul>
+    <AdminDashboard></AdminDashboard>
+    {/* <ul>{HTMLAdmins}</ul> */}
     </>
   )
 }
